1e86a808507b21b4339a46fde1b6943bb6245d8c
shu-kob <shukobuchi+github@gmail.com>
2020-11-09 23:07:56 +0900

    Add index view and so on

---
+++ app/controllers/wallet_controller.rb
@@ -0,0 +1,26 @@
+require 'bitcoin'
+require 'net/http'
+require 'json'
+Bitcoin.chain_params = :testnet
+RPCUSER="hoge"
+RPCPASSWORD="hoge"
+HOST="localhost"
+PORT=38332
+require 'rqrcode'
+
+class WalletController < ApplicationController
+  def index
+    @balance = bitcoinRPC('getbalance',[])
+    render template: 'wallet/index'
+  end
+
+  private
+	def bitcoinRPC(method,param)
+		http = Net::HTTP.new(HOST, PORT)
+    request = Net::HTTP::Post.new('/')
+    request.basic_auth(RPCUSER,RPCPASSWORD)
+    request.content_type = 'application/json'
+    request.body = {method: method, params: param, id: 'jsonrpc'}.to_json
+    JSON.parse(http.request(request).body)["result"]
+  end
+end
